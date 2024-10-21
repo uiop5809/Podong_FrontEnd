27d853d135b73036bc3c8dd0fcb106f64ba3faf2
parkyejin <uiop5809@naver.com>
2024-10-21 09:18:26 +0900

    chore(#1): Router 설정

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div>메인페이지</div>
-    </>
-  );
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import Router from "./Router.jsx";
 import GlobalStyle from "./styles/GlobalStyles.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyle />
-    <App />
+    <Router />
   </StrictMode>
 );
