1756226659d9a78090d210733a9a6d90afa10457
parkyejin <uiop5809@naver.com>
2024-10-29 01:33:06 +0900

    feat(#3): Geolocation API로 사용자 위치 가져오기

--- src/components/walking/WalkingMap.jsx
+++ src/components/walking/WalkingMap.jsx
@@ -1,24 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const { kakao } = window;
 
 const WalkingMap = () => {
+  const [latitude, setLatitude] = useState(33.450701);
+  const [longitude, setLongitude] = useState(126.570667);
+
   useEffect(() => {
-    const mapContainer = document.getElementById("map");
-    const mapOptions = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
-    };
+    // Geolocation API로 사용자 위치 가져오기
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
 
-    const kakaoMap = new kakao.maps.Map(mapContainer, mapOptions);
-  }, []);
+          const mapContainer = document.getElementById("map");
+          const mapOptions = {
+            center: new kakao.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+            ),
+            level: 3,
+          };
 
-  return (
-    <>
-      <MapContainer id="map" />
-    </>
-  );
+          // 카카오 지도 생성
+          new kakao.maps.Map(mapContainer, mapOptions);
+        },
+        (error) => {
+          console.error("Error fetching location: ", error);
+          // 위치 정보 가져오기 실패 시 기본 좌표 사용
+          const mapContainer = document.getElementById("map");
+          const mapOptions = {
+            center: new kakao.maps.LatLng(latitude, longitude),
+            level: 3,
+          };
+          new kakao.maps.Map(mapContainer, mapOptions);
+        }
+      );
+    } else {
+      alert("위치는 이 브라우저에서 지원되지 않습니다.");
+    }
+  }, [latitude, longitude]);
+
+  return <MapContainer id="map" />;
 };
 
 export default WalkingMap;
@@ -30,6 +55,5 @@ const MapContainer = styled.div`
   @media (max-width: 500px) {
     width: 100vw;
   }
-
   height: 400px;
 `;
