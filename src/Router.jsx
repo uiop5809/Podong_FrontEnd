4701bf2a6d7feed1ce5d471076e50857b8949180
subinsong01 <haely97@naver.com>
2024-10-28 10:23:50 +0900

    feat(#7): update mypage

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,34 +3,42 @@ import MainPage from './pages/MainPage/MainPage.jsx';
 import Nav from './components/common/Nav.jsx';
 import Footer from './components/common/Footer';
 import ScrollTop from './components/common/ScrollTop.jsx';
-<<<<<<< Updated upstream
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
-=======
 import PGpay from './pages/PaymentPage/PGpay.jsx';
 import Payment from './pages/PaymentPage/Payment.jsx';
 import PaymentEnd from './pages/PaymentPage/PaymentEnd.jsx';
->>>>>>> Stashed changes
 import PetItemPage from './pages/PetItemPage/PetItemPage.jsx';
 import PetItemListPage from './pages/PetItemPage/PetItemListPage.jsx';
 import PetItemDetailPage from './pages/PetItemPage/PetItemDetailPage.jsx';
+import UserRegisterPage from './pages/RegisterPage/UserRegisterPage.jsx';
+import PetEditPage from './pages/MyPage/PetEditPage.jsx';
+import UserEditPage from './pages/MyPage/UserEditPage.jsx';
+import MyPage from './pages/MyPage/MyPage.jsx';
+import RegisterMissingSavePage from './pages/MyPage/RegisterMissingSavePage.jsx';
+import RegisterMissing from './pages/MyPage/RegisterMissing.jsx';
+import PetRegisterPage from './pages/RegisterPage/PetRegisterPage.jsx';
 
 function Router() {
   return (
     <BrowserRouter>
       <div className="Router">
-        {/* <Nav /> */}
+        <Nav />
         <Routes>
           <Route path="/" element={<PaymentEnd/>} />
           <Route path="/" element={<MainPage />} />
-<<<<<<< Updated upstream
           <Route path="/Login" element={<LoginPage />} />
-=======
->>>>>>> Stashed changes
           <Route path="/petitem" element={<PetItemPage />} />
           <Route path="/petitemList" element={<PetItemListPage />} />
           <Route path="/petitemDetail/:no" element={<PetItemDetailPage/>} />
+          <Route path="/user-register" element={<UserRegisterPage />} />
+          <Route path="/petRegister" element={<PetRegisterPage/>} />
+          <Route path="/editUserRegister" element={<UserEditPage />} />
+          <Route path="/editPetRegister" element={<PetEditPage/>} />
+          <Route path="/mypage" element={<MyPage />}/>
+          <Route path="/missingSave" element={<RegisterMissingSavePage />} /> 
+          <Route path="/missingRegister" element={<RegisterMissing/>} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
         <ScrollTop />
       </div>
     </BrowserRouter>
@@ -38,3 +46,4 @@ function Router() {
 }
 
 export default Router;
+
---
+++ src/pages/MyPage/RegisterMissingSavePage.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled from "styled-components";
+
+const ScrollableContainer = styled.div`
+  max-height: 100%;
+  overflow-y: auto; 
+  border: 1px solid #ddd;
+  margin: 64px 0;
+`; // 스크롤 
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%; 
+  margin-top: 30%;
+  margin-left: 5%;
+  overflow-y: auto; 
+`; // 전체 컨테이너 
+
+const MentionContainer = styled.div`
+  text-align: center;
+  margin-bottom: 50px;
+`; 
+
+const SaveMention = styled.div`
+  font-weight: bold;
+  font-size: 15px;
+  margin-bottom: 30px;
+  text-align: center;
+`; 
+
+const SaveConfirm = styled.span`
+  font-size: 17px;
+  font-weight: bold;
+  word-break: keep-all;
+  color: #FF6E00;
+  line-height: 1.7;
+`;  
+
+const MentionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius:5px;
+  height: 250px;
+  margin-bottom: 20%;
+  text-align: center;
+  padding-top:30px;
+  padding:20px;
+  background-color: #F5F5F5;
+`; 
+
+const Recommendation = styled.div`
+  font-size:15px;
+  font-weight:bold;
+  margin-bottom: 30px;
+`
+
+const RecommendationDescription = styled.div`
+  font-size: 12px;
+  font-weight: normal;
+  text-align: center;
+  line-height: 1.4;
+  word-break: keep-all;
+  margin-bottom: 30px;
+`
+
+const AlertRecommendation = styled.span`
+  font-size: 10px;
+  font-weight: bold;
+  color: #FF0000;
+`
+
+const RegisterMissingSavePage = () => {
+  return (
+    <ScrollableContainer>
+      <Container>
+        <MentionContainer>
+          <SaveMention>
+            정보가 저장되었습니다.<br />
+          </SaveMention>
+          <SaveConfirm>
+            아이가 빠르게 가족의 품으로 돌아갈 수 있도록 
+            발바닥천국이 최선을 다하겠습니다.
+          </SaveConfirm>
+        </MentionContainer>
+        <MentionBox>
+          <Recommendation>
+            추천
+          </Recommendation>
+          <RecommendationDescription>
+          다른 견주가 내가 강아지를 잃어버린 위치 주변에 있을 때, 알림을 보내드려요. 알림을 받은 견주는 해당 위치를 주의 깊게 살펴보게 됩니다.<br /><br />
+          아이를 발견하면, 즉시 강아지가 세상을 지배한다님에게 발견 알림이 갑니다.
+        </RecommendationDescription>
+        <AlertRecommendation>
+          알림을 켜두는 것을 추천드려요
+        </AlertRecommendation>
+      </MentionBox>
+    </Container>  
+    </ScrollableContainer>
+  )
+}
+
+export default RegisterMissingSavePage
