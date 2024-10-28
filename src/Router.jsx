3808499e882e9e2b41ae2b136c04747fb309c55d
Suh-code <yunkisuh@naver.com>
2024-10-28 10:08:45 +0900

    feat(#6):1차 결제, 주문페이지

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,7 +3,13 @@ import MainPage from './pages/MainPage/MainPage.jsx';
 import Nav from './components/common/Nav.jsx';
 import Footer from './components/common/Footer';
 import ScrollTop from './components/common/ScrollTop.jsx';
+<<<<<<< Updated upstream
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
+=======
+import PGpay from './pages/PaymentPage/PGpay.jsx';
+import Payment from './pages/PaymentPage/Payment.jsx';
+import PaymentEnd from './pages/PaymentPage/PaymentEnd.jsx';
+>>>>>>> Stashed changes
 import PetItemPage from './pages/PetItemPage/PetItemPage.jsx';
 import PetItemListPage from './pages/PetItemPage/PetItemListPage.jsx';
 import PetItemDetailPage from './pages/PetItemPage/PetItemDetailPage.jsx';
@@ -12,15 +18,19 @@ function Router() {
   return (
     <BrowserRouter>
       <div className="Router">
-        <Nav />
+        {/* <Nav /> */}
         <Routes>
+          <Route path="/" element={<PaymentEnd/>} />
           <Route path="/" element={<MainPage />} />
+<<<<<<< Updated upstream
           <Route path="/Login" element={<LoginPage />} />
+=======
+>>>>>>> Stashed changes
           <Route path="/petitem" element={<PetItemPage />} />
           <Route path="/petitemList" element={<PetItemListPage />} />
           <Route path="/petitemDetail/:no" element={<PetItemDetailPage/>} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
         <ScrollTop />
       </div>
     </BrowserRouter>
