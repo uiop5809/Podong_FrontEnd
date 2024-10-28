323574da816fcb61f5cb78c519d7aaa1f9d3194f
LeeSunghuck <hello.git.anaolfive@gmail.com>
2024-10-28 09:36:16 +0900

    feat(#8):update

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import MainPage from './pages/MainPage/MainPage.jsx';
 import Nav from './components/common/Nav.jsx';
 import Footer from './components/common/Footer';
 import ScrollTop from './components/common/ScrollTop.jsx';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Nav />
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/Login" element={<LoginPage />} />
         </Routes>
         <Footer />
         <ScrollTop />
