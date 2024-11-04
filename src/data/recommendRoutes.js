a8a6e9a6d4589c2afa5038cb7f4e34cabf396afc
parkyejin <uiop5809@naver.com>
2024-11-05 02:43:49 +0900

    feat(#55): 산책경로 추천

---
+++ src/data/recommendRoutes.js
@@ -0,0 +1,98 @@
+export const RecommendRoutes = [
+  {
+    id: 1,
+    name: "한강공원 코스",
+    distance: "3.5km",
+    time: "45분",
+    routeData: {
+      center: { lat: 37.5665, lng: 126.978 },
+      start: { lat: 37.566, lng: 126.977 },
+      end: { lat: 37.567, lng: 126.979 },
+      path: [
+        { lat: 37.566, lng: 126.977 },
+        { lat: 37.5665, lng: 126.978 },
+        { lat: 37.567, lng: 126.979 },
+      ],
+    },
+  },
+  {
+    id: 5,
+    name: "서울숲 산책로",
+    distance: "2.8km",
+    time: "35분",
+    routeData: {
+      center: { lat: 37.5446, lng: 127.0379 },
+      start: { lat: 37.5441, lng: 127.0374 },
+      end: { lat: 37.5451, lng: 127.0384 },
+      path: [
+        { lat: 37.5441, lng: 127.0374 },
+        { lat: 37.5446, lng: 127.0379 },
+        { lat: 37.5451, lng: 127.0384 },
+      ],
+    },
+  },
+  {
+    id: 6,
+    name: "청계천 산책로",
+    distance: "3.9km",
+    time: "50분",
+    routeData: {
+      center: { lat: 37.5696, lng: 127.0214 },
+      start: { lat: 37.5691, lng: 127.0209 },
+      end: { lat: 37.5701, lng: 127.0219 },
+      path: [
+        { lat: 37.5691, lng: 127.0209 },
+        { lat: 37.5696, lng: 127.0214 },
+        { lat: 37.5701, lng: 127.0219 },
+      ],
+    },
+  },
+  {
+    id: 7,
+    name: "월드컵공원 하늘공원 코스",
+    distance: "4.5km",
+    time: "60분",
+    routeData: {
+      center: { lat: 37.5676, lng: 126.885 },
+      start: { lat: 37.5671, lng: 126.8845 },
+      end: { lat: 37.5681, lng: 126.8855 },
+      path: [
+        { lat: 37.5671, lng: 126.8845 },
+        { lat: 37.5676, lng: 126.885 },
+        { lat: 37.5681, lng: 126.8855 },
+      ],
+    },
+  },
+  {
+    id: 8,
+    name: "석촌호수 산책로",
+    distance: "2.5km",
+    time: "30분",
+    routeData: {
+      center: { lat: 37.5085, lng: 127.1052 },
+      start: { lat: 37.508, lng: 127.1047 },
+      end: { lat: 37.509, lng: 127.1057 },
+      path: [
+        { lat: 37.508, lng: 127.1047 },
+        { lat: 37.5085, lng: 127.1052 },
+        { lat: 37.509, lng: 127.1057 },
+      ],
+    },
+  },
+  {
+    id: 10,
+    name: "안양천 산책로",
+    distance: "5.5km",
+    time: "70분",
+    routeData: {
+      center: { lat: 37.5172, lng: 126.8664 },
+      start: { lat: 37.5167, lng: 126.8659 },
+      end: { lat: 37.5177, lng: 126.8669 },
+      path: [
+        { lat: 37.5167, lng: 126.8659 },
+        { lat: 37.5172, lng: 126.8664 },
+        { lat: 37.5177, lng: 126.8669 },
+      ],
+    },
+  },
+];
