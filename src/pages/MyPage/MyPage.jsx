f16cc6b8d05966f0bbe3af5c1699a6c1276779e4
LeeSunghuck <hello.git.anaolfive@gmail.com>
2024-11-04 23:49:46 +0900

    feat(#8): 소셜로그인

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -6,7 +6,7 @@ import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { images } from '../../components/Images';
-import axios from 'axios';
+
 
 const ScrollableContainer = styled.div`
   max-height: 100%;
